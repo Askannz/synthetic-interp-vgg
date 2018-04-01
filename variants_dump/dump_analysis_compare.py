89271a81ffea297696909b0bb59fab55ead2b2d5
Robin Lange <robin.langenc@gmail.com>
2018-04-01 19:09:52 +0800

    Dump compare without diff + bar graph

--- variants_dump/dump_analysis_compare.py
+++ variants_dump/dump_analysis_compare.py
@@ -3,24 +3,6 @@ import numpy as np
 import pickle
 import matplotlib.pyplot as plt
 
-
-def make_disp(data):
-
-    disp = {}
-    for layer in LAYERS:
-
-        if data[layer].ndim == 4:
-            max_layer = np.max(np.max(data[layer], axis=1), axis=1)
-        else:
-            max_layer = data[layer]
-
-        cumul = np.sum(max_layer, axis=0)
-
-        disp[layer] = np.abs(cumul)
-
-    return disp
-
-
 PATH_ORIGINAL = sys.argv[1]
 PATH_VARIANT = sys.argv[2]
 
@@ -30,24 +12,38 @@ LAYERS = original_results["layers"]
 BASENAME = variant_results["basename"]
 VARIANT = variant_results["variant"]
 
-diffs = {}
+original = {}
+variant = {}
 
 for layer in LAYERS:
-    diffs[layer] = []
+    original[layer] = []
+    variant[layer] = []
     for filename in original_results["results"].keys():
         layer_original = original_results["results"][filename][1][layer]
         layer_variant = variant_results["results"][filename][1][layer]
-        diffs[layer].append(layer_original - layer_variant)
 
-    diffs[layer] = np.array(diffs[layer])
+        if layer_original.ndim == 3:
+            layer_original = np.max(np.max(layer_original, axis=0), axis=0)
+            layer_variant = np.max(np.max(layer_variant, axis=0), axis=0)
 
-diffs_disp = make_disp(diffs)
+        original[layer].append(layer_original)
+        variant[layer].append(layer_variant)
+
+    original[layer] = np.array(original[layer])
+    variant[layer] = np.array(variant[layer])
+
 
 nblayers = len(LAYERS)
+BAR_WIDTH = 0.1
 
 for layer in LAYERS:
+    original_max = np.max(original[layer], axis=0)
+    variant_max = np.max(variant[layer], axis=0)
+    indices_sorted = np.argsort(original_max)
     plt.figure()
-    plt.plot(diffs_disp[layer])
+    plt.bar(np.arange(original_max.size), original_max[indices_sorted], 1)
+    plt.bar(np.arange(original_max.size), variant_max[indices_sorted], 1)
+    plt.grid()
     plt.title(layer)
 
 plt.show()
