42e949977c7f49f562463fe1e295e207f1ff511c
Robin Lange <robin.langenc@gmail.com>
2018-04-10 17:21:20 +0800

    Display improvements

--- variants_display.py
+++ variants_display.py
@@ -24,9 +24,11 @@ for basename in results["results"].keys():
         classes_probas = np.stack([original_classes_probas, variant_classes_probas], axis=1)
         classes_probas_sorted = classes_probas[classes_probas[:, 0].argsort()]
 
-        plt.plot(classes_probas_sorted[:, 0], marker='+', label='original')
-        plt.plot(classes_probas_sorted[:, 1], marker='*', label=variant)
+        plt.bar(np.arange(classes_probas_sorted.shape[0]), classes_probas_sorted[:, 0], 0.5, label='original')
+        plt.bar(np.arange(classes_probas_sorted.shape[0]) + 0.5, classes_probas_sorted[:, 1], 0.5, label=variant)
         plt.legend(loc='upper left')
         plt.grid()
-        plt.title("%s %s" % (basename, variant))
+        plt.xlabel("View number")
+        plt.ylabel("Probability for true class")
+        plt.title("Results for %s %s, sorted by original probabilities" % (basename, variant))
         plt.show()
--- variants_dump/dump_analysis_compare.py
+++ variants_dump/dump_analysis_compare.py
@@ -1,16 +1,14 @@
 import sys
 import numpy as np
-import pickle
+import h5py
 import matplotlib.pyplot as plt
 
 PATH_ORIGINAL = sys.argv[1]
 PATH_VARIANT = sys.argv[2]
 
-original_results = pickle.load(open(PATH_ORIGINAL, "rb"))
-variant_results = pickle.load(open(PATH_VARIANT, "rb"))
-LAYERS = original_results["layers"]
-BASENAME = variant_results["basename"]
-VARIANT = variant_results["variant"]
+original_results = h5py.File(PATH_ORIGINAL, 'r')
+variant_results = h5py.File(PATH_VARIANT, 'r')
+LAYERS = list(original_results.keys())
 
 original = {}
 variant = {}
@@ -18,9 +16,9 @@ variant = {}
 for layer in LAYERS:
     original[layer] = []
     variant[layer] = []
-    for filename in original_results["results"].keys():
-        layer_original = original_results["results"][filename][1][layer]
-        layer_variant = variant_results["results"][filename][1][layer]
+    for i in range(original_results[layer].shape[0]):
+        layer_original = original_results[layer][i]
+        layer_variant = variant_results[layer][i]
 
         if layer_original.ndim == 3:
             layer_original = np.max(np.max(layer_original, axis=0), axis=0)
@@ -37,12 +35,14 @@ nblayers = len(LAYERS)
 BAR_WIDTH = 0.1
 
 for layer in LAYERS:
-    original_max = np.max(original[layer], axis=0)
-    variant_max = np.max(variant[layer], axis=0)
-    indices_sorted = np.argsort(original_max)
+    original_mean = np.mean(original[layer], axis=0)
+    variant_mean = np.mean(variant[layer], axis=0)
+    indices_sorted = np.argsort(original_mean)
     plt.figure()
-    plt.bar(np.arange(original_max.size), original_max[indices_sorted], 1)
-    plt.bar(np.arange(original_max.size), variant_max[indices_sorted], 1)
+    plt.bar(np.arange(original_mean.size), original_mean[indices_sorted], 0.5)
+    plt.bar(np.arange(original_mean.size) + 0.5, variant_mean[indices_sorted], 0.5)
+    plt.xlabel('Filter id')
+    plt.ylabel('Activation')
     plt.grid()
     plt.title(layer)
 
