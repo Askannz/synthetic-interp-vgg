6aa30514901f7da559740aabf3b5323cecfa540c
Robin Lange <robin.langenc@gmail.com>
2018-04-17 17:56:11 +0800

    Variants display with importance

--- variants/variants_display.py
+++ variants/variants_display.py
@@ -5,6 +5,11 @@ import matplotlib.pyplot as plt
 
 RESULTS_PATH = sys.argv[1]
 
+importance_data = None
+if len(sys.argv) > 2:
+    IMPORTANCES_PATH = sys.argv[2]
+    importance_data = json.load(open(IMPORTANCES_PATH, "r"))
+
 results = json.load(open(RESULTS_PATH, "r"))
 
 for basename in results["results"].keys():
@@ -22,13 +27,28 @@ for basename in results["results"].keys():
         variant_classes_probas = np.sum(variant_classes_probas, axis=1)
 
         classes_probas = np.stack([original_classes_probas, variant_classes_probas], axis=1)
-        classes_probas_sorted = classes_probas[classes_probas[:, 0].argsort()]
 
-        plt.bar(np.arange(classes_probas_sorted.shape[0]), classes_probas_sorted[:, 0], 0.5, label='original')
-        plt.bar(np.arange(classes_probas_sorted.shape[0]) + 0.5, classes_probas_sorted[:, 1], 0.5, label=variant)
+        locs = np.arange(classes_probas.shape[0])
+
+        if importance_data is None:
+            classes_probas_sorted = classes_probas[classes_probas[:, 0].argsort()]
+            labels = locs + 1
+        else:
+            render_filenames = results["results"][basename]["renders_filenames"]
+            importances_dict = importance_data["results"][basename][variant]
+            importances = np.array([importances_dict[f] for f in render_filenames])
+
+            indices_sorted = importances.argsort()[::-1]
+            classes_probas_sorted = classes_probas[indices_sorted]
+            importances_norm = importances / max(1e-7, np.max(importances))
+            labels = ["%2.0f" % (100 * v) for v in importances_norm[indices_sorted]]
+
+        plt.bar(locs, classes_probas_sorted[:, 0], 0.5, label='original', align='center')
+        plt.bar(locs + 0.5, classes_probas_sorted[:, 1], 0.5, label=variant, align='center')
         plt.legend(loc='upper left')
         plt.grid()
         plt.xlabel("View number")
         plt.ylabel("Probability for true class")
+        plt.xticks(locs + 0.25, labels)
         plt.title("Results for %s %s, sorted by original probabilities" % (basename, variant))
         plt.show()
